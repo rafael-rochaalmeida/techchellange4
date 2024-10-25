5b8b8a10477c1100ba12c62e02825abc2202c010
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2024-10-24 21:25:45 -0300

    initial commit

---
+++ gerenciamento-de-produtos/src/test/java/br/com/gerenciamento/produtos/GerenciamentoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciamento.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
