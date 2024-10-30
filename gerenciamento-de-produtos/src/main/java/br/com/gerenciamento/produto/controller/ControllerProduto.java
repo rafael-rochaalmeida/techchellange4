370651eb3e2db07ff98e610be82d597c985576b3
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2024-10-29 22:33:33 -0300

    post produtos

--- gerenciamento-de-produtos/src/main/java/br/com/gerenciamento/produto/controller/ControllerProduto.java
+++ gerenciamento-de-produtos/src/main/java/br/com/gerenciamento/produto/controller/ControllerProduto.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,7 @@ import br.com.gerenciamento.produto.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/produto")
-public class ProdutoController {
+public class ControllerProduto {
 
     @Autowired
     private ProdutoRepository produtoRepository;
@@ -21,6 +23,11 @@ public class ProdutoController {
     @GetMapping
     public ResponseEntity<List<Produto>> getProdutos(){
         return ResponseEntity.status(200).body(produtoRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> postProduto(@RequestBody Produto produto){
+        return ResponseEntity.status(201).body(produtoRepository.save(produto));
     }
 
 }
